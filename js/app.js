8b3615bfd49d40db30ff8554d604e033b1dd8ae3
Jahir Barojas <j.jahir@msn.com>
2016-10-31 19:31:41 -0600

    Update with color message and empty item validation

--- js/app.js
+++ js/app.js
@@ -8,18 +8,33 @@ LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
     $scope.lunchText = "";
     $scope.message = "";
+    $scope.colorData = 0;
     $scope.checkLunch = function() {
       var arrayLunch = [];
       arrayLunch = $scope.lunchText.split(',');
-      console.log(arrayLunch.length);
-      if($scope.lunchText != "") {
-        if(arrayLunch.length <= 3) {
+      var numItems = getNumItems(arrayLunch);
+      //console.log("num items array split: "+arrayLunch.length);
+      if($scope.lunchText.trim() != "") {
+        if(numItems <= 3) {
+          $scope.colorData = 1;
           $scope.message = "Enjoy!";
-        } else if(arrayLunch.length >= 3) {
+        } else if(numItems > 3) {
           $scope.message = "Too much!";
         }
       } else {
         $scope.message = "Please enter data first";
+        $scope.colorData = 2;
+      }
+
+      function getNumItems(arrayItems) {
+        var countItem = 0;
+        angular.forEach(arrayItems, function(item) {
+          if(item.trim() != "") {
+            countItem = countItem + 1;
+          }
+        });
+        //console.info("Num items: "+countItem);
+        return countItem;
       }
 
     }
